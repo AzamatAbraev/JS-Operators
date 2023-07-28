26f12783497ad44ec8ba7050ca783a2ff974f40a
AzamatAbraev <azamatabraev07@gmail.com>
2023-07-28 11:34:31 +0500

    JS Operators

---
+++ operators.js
@@ -0,0 +1,349 @@
+// Boolean1
+
+// let A = 6;
+
+// let res = A % 2 == 0;
+
+// console.log(res);
+
+// Boolean2
+
+// let A = 1;
+
+// let B = 0;
+
+// let res = A >= 0 || B < -2;
+
+// console.log(res);
+
+// Boolean3
+
+// let A = 3;
+
+// let B = 5;
+
+// let C = 6;
+
+// let res = A < B && B < C;
+
+// console.log(res);
+
+// Boolean4
+
+// let A = 5;
+
+// let B = 5;
+
+// let res = A % 2 == 1 && B % 2 == 1;
+
+// console.log(res);
+
+// Boolean5
+
+// let A = 2;
+
+// let B = 4;
+
+// let res = A % 2 == 1 || B % 2 == 1;
+
+// console.log(res);
+
+// Boolean6
+
+// let A = 1;
+
+// let B = 3;
+
+// let res = (A + B) % 2 == 1;
+
+// console.log(res);
+
+// Boolean7
+
+// let A = 2;
+
+// let B = 5;
+
+// let C = 4;
+
+// let res = A > 0 && B > 0 && C > 0;
+
+// console.log(res);
+
+// Boolean8
+
+// let A = -5;
+
+// let B = -3;
+
+// let C = -2;
+
+// let res =
+//   ((A < 0 && B < 0) || (A < 0 && C < 0)) && (A > 0 || B > 0 || C > 0);
+
+// console.log(res);
+
+// Boolean9
+
+// let A = -2;
+
+// let B = -4;
+
+// let C = 3;
+
+// let res = ((A > 0 && B > 0) || (A > 0 && C > 0)) && (A < 0 || B < 0 || C < 0);
+
+// console.log(res);
+
+// Boolean10
+
+// let ab = 8;
+
+// let res = ab > 9 && ab % 2 == 0;
+
+// console.log(res);
+
+// Boolean11
+
+// let abc = -120;
+
+// let res = abc > 99 && abc % 2 == 1;
+
+// console.log(res);
+
+// Boolean12
+
+// let A = -1;
+
+// let B = 0;
+
+// let C = -5;
+
+// let res = A + B == 0 || A + C == 0 || B + C == 0;
+
+// console.log(res);
+
+// Boolean13
+
+// let abc = 359;
+
+// let bc = abc % 100;
+
+// let c = bc % 10;
+
+// let b = (bc - c) / 10;
+
+// let a = (abc - bc) / 100;
+
+// let res = a < b && b < c;
+
+// console.log(res);
+
+// Boolean14
+
+// let abc = 129;
+
+// let bc = abc % 100;
+
+// let c = bc % 10;
+
+// let b = (bc - c) / 10;
+
+// let a = (abc - bc) / 100;
+
+// let res = (a < b && b < c) || (a > b && b > c) ;
+
+// console.log(res);
+
+// Boolean15
+
+// let abc = 525;
+
+// let bc = abc % 100;
+
+// let c = bc % 10;
+
+// let b = (bc - c) / 10;
+
+// let a = (abc - bc) / 100;
+
+// let res = a == c && (b > a || b < a);
+
+// console.log(res);
+
+// Boolean16
+
+// let x1 = 4;
+
+// let y1 = 2;
+
+// let x2 = 1;
+
+// let y2 = 7;
+
+// let res = ((Math.abs(x2 - x1)) % 2 == 1 && (Math.abs(y2 - y1)) % 2 == 1);
+
+// console.log(res);
+
+// Boolean17
+
+// let x1 = 2;
+
+// let y1 = 3;
+
+// let x2 = 1;
+
+// let y2 = 8;
+
+// let res = (x1 == x2) || (y1 == 2);
+
+// console.log(res);
+
+// Boolean18
+
+// let x1 = 2;
+
+// let y1 = 4;
+
+// let x2 = 2;
+
+// let y2 = 3;
+
+// let res =
+//   (x1 == x2 || y2 == y1) && (Math.abs(x2 - x1) == 1 &&
+//     Math.abs(y2 - y1) == 1 &&
+//     Math.abs(x2 - x1) / Math.abs(y2 - y1) == 1);
+
+// console.log(res);
+
+// Operator1
+
+// let A = 2;
+
+// let B = 4;
+
+// let Y = - ((B / 2) * (A ** 2 + B ** 2));
+
+// console.log(Y);
+
+// Operator2
+
+// let A = 1;
+
+// let B = 3;
+
+// let C = 3;
+
+// let D = 5;
+
+// let Y = ((A + B) / (C + D)) ** 2 + B ** 2;
+
+// console.log(Y);
+
+// Operator3
+
+// let A = 1;
+
+// let D = 2;
+
+// let C = 3;
+
+// let Y = 2 * (A ** D + ((4 * C ** 2) / 3));
+
+// console.log(Y);
+
+// Operator4
+
+// let A = 1;
+
+// let B = 2;
+
+// let C = 3;
+
+// let D = 4;
+
+// let Y = (1 / A ** 2) * ((B / 10) ** 3) * (C + D) ** 2;
+
+// console.log(Y);
+
+// Operator5
+
+// let A = 1;
+
+// let B = 2;
+
+// let C = 3;
+
+// let D = 4;
+
+// let Y = ((A / B - 1) / (C / (D - 1)));
+
+// console.log(Y);
+
+// Operator6
+
+// let A = 1;
+
+// let B = 2;
+
+// let C = 3;
+
+// let D = 4;
+
+// let Y = (A + B) / (C + (D / (A + C)));
+
+// console.log(Y);
+
+// Operator7
+
+// let A = 1;
+
+// let B = 2;
+
+// let C = 3;
+
+// let D = 4;
+
+// let Y = ((A + B + C) ** 2 - (A - B - C) ** 2) ** (1/2);
+
+// Operator8
+
+// let A = 1;
+
+// let B = 2;
+
+// let C = 3;
+
+// let D = 4;
+
+// let Y = ((A ** 2 + B ** 2 + C ** 2) / (A * B * C));
+
+// console.log(Y);
+
+// Operator9
+
+// let A = 1;
+
+// let B = 2;
+
+// let C = 3;
+
+// let D = 4;
+
+// let Y = (A * ((D ** 3) / 3)) + (B  * ((C ** 2) / 2));
+
+// console.log(Y);
+
+// Operator10
+
+// let A = 1;
+
+// let B = 2;
+
+// let C = 3;
+
+// let D = 4;
+
+// let Y = Math.abs(A / B - B /C) + (A + C) ** (1/4);
+
+// console.log(Y);
